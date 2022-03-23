68ac13ff5f535ef7a15598d93655727d82e9e482
Manideep <17101992m@gmail.com>
2022-03-24 00:31:50 +0530

    Project discription changed.

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -2,14 +2,16 @@ import { Images } from "../helpers/imageData";
 
 function Projects() {
   return (
-    <div id="projects" className="flex items-center justify-center mx-3 lg:w-3/4 self-center pt-3">
+    <div
+      id="projects"
+      className="flex items-center w-full justify-center mx-3 lg:w-3/4 self-center pt-3"
+    >
       <section className="w-full border-solid mt-10 p-5 mx-5 bg-bg-sec rounded-2xl space-y-8">
         <h3 className=" inline-block text-3xl font-baloo2 text-text shadow-lg shadow-backcolor">
           Projects
         </h3>
         <p className="text-xl font-baloo2 text-text-secondary">
-          I’ve had the pleasure of working with multiple Fortune 500 companies,
-          designing and implementing both frontend and backend
+          A small gallery of recent projects chosen by me.
         </p>
 
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-6 ">
