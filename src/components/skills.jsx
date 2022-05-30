0b1427e4fb6028f4d086569beb2fa712cfd4152c
Manideep <17101992m@gmail.com>
2022-05-31 02:01:05 +0530

    skill component added

---
+++ src/components/skills.jsx
@@ -0,0 +1,28 @@
+import { SkillData } from "../helpers/skillData";
+
+const Skills = () => {
+  return (
+    <div
+      id="skills"
+      className="flex items-center w-full h-80 justify-center mx-3 self-center pt-20 mt-5"
+    >
+      <h3 className=" inline-block text-3xl font-baloo2 text-text shadow-lg shadow-backcolor rounded-lg px-2 self-start">
+          Skills
+        </h3>
+      <div className="skillItems flex justify-center text-center gap-5 item-center">
+        {SkillData.map((item, key) => {
+          return (
+            <div className="flex justify-center flex-col items-center bg-bg-sec w-36 h-36 rounded-md" key={key}>
+              {item.logo}
+              <p className="text-xl font-baloo2 text-text-secondary">
+                {item.name}
+              </p>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Skills;
